49eaaf0e673f6182680400400a05f48890c5a613
Jean-Francois Larouche <rouche@gmail.com>
2020-10-14 11:39:26 -0400

    Intro Artificial Neuron Network

--- Formations/GFT-IntroDeepLearning/README.md
+++ Formations/GFT-IntroDeepLearning/README.md
@@ -3,3 +3,4 @@
 
 http://colab.research.google.com/drive/1YMeDdEkge5YDvVBFBYDnoLl8Flu6v4oX?usb=sharing
 
+http://playground.tensorflow.org/#activation=tanh&batchSize=10&dataset=gauss&regDataset=reg-plane&learningRate=0.03&regularizationRate=0&noise=0&networkShape=1&seed=0.64578&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false
--- Formations/GFT-IntroDeepLearning/introdeeplearning/and_or_not.py
+++ Formations/GFT-IntroDeepLearning/introdeeplearning/and_or_not.py
@@ -5,7 +5,7 @@ def compute_output_validate(weeight1, weight2, bias, inputs, correct):
     outputs = []
 
     # Generate and check output
-    for test_input, correct_output in zip(test_inputs, correct_outputs):
+    for test_input, correct_output in zip(test_inputs, correct):
         linear_combination = weight1 * test_input[0] + weight2 * test_input[1] + bias
         output = int(linear_combination >= 0)
         is_correct_string = 'Yes' if output == correct_output else 'No'
@@ -20,7 +20,7 @@ def compute_output_validate(weeight1, weight2, bias, inputs, correct):
         print('You got {} wrong.  Keep trying!\n'.format(num_wrong))
     print(output_frame.to_string(index=False))
 
-
+# And
 weight1 = 1
 weight2 = 1
 bias = -1.5
@@ -30,20 +30,20 @@ correct_outputs = [False, False, False, True]
 
 compute_output_validate(weight1, weight2, bias, test_inputs, correct_outputs)
 
+# Or
 weight1 = 1
 weight2 = 1
 bias = -0.5
 
-test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
 correct_outputs = [False, True, True, True]
 
 compute_output_validate(weight1, weight2, bias, test_inputs, correct_outputs)
 
+# Not, work only on input 1
 weight1 = -1
 weight2 = 0
 bias = 0
 
-test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
 correct_outputs = [True, True, False, False]
 
 compute_output_validate(weight1, weight2, bias, test_inputs, correct_outputs)
